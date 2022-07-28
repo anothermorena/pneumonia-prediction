519132b1267419805c861d411e129892c48dae85
Morena <onalepeloo@live.com>
2022-07-28 10:26:26 +0200

    added the google cloud function api end point to the client/axios

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 //set the url for the entire application
-const BASE_URL = 'http://localhost:8000';
+const BASE_URL = 'https://us-central1-pneumonia-prediction-app.cloudfunctions.net/predict';
 
 export default axios.create({
     baseURL: BASE_URL
